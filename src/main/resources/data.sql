59b7dbfaed4498411d5f67f1e07932a67aef3eb7
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-04-18 16:28:31 +0900

    fix: insert 쿼리 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,24 +1,23 @@
-insert into Team(`name`) values('team1');
-insert into Team(`name`) values('team2');
-insert into Team(`name`) values('team3');
-insert into Team(`name`) values('team4');
-insert into Team(`name`) values('team5');
+insert into Team(name) values('team1');
+insert into Team(name) values('team2');
+insert into Team(name) values('team3');
+insert into Team(name) values('team4');
+insert into Team(name) values('team5');
 
+insert into Book(name) values('book1');
+insert into Book(name) values('book2');
+insert into Book(name) values('book3');
+insert into Book(name) values('book4');
 
-insert into Book(`name`) values('book1');
-insert into Book(`name`) values('book2');
-insert into Book(`name`) values('book3');
-insert into Book(`name`) values('book4');
+insert into Member(name, age, team_id, city, gu, dong) values('jino', 24,1, 'Seoul', 'mapo', 'sang-su');
+insert into Member(name, age, team_id, city, gu, dong) values('seoungbum', 24,1, 'Seoul', 'mapo', 'sang-su');
+insert into Member(name, age, team_id, city, gu, dong) values('wiji', 24,2, 'Seoul', 'mapo', 'sang-su');
+insert into Member(name, age, team_id, city, gu, dong) values('dosik', 24,2, 'Seoul', 'mapo', 'sang-su');
+insert into Member(name, age, team_id, city, gu, dong) values('mingi', 24,3, 'Seoul', 'mapo', 'sang-su');
+insert into Member(name, age, team_id, city, gu, dong) values('youngjin', 24,3, 'Seoul', 'mapo', 'sang-su');
 
-insert into Member(`name`, `age`, `team_id`) values('jino', 24,1);
-insert into Member(`name`, `age`, `team_id`) values('seoungbum', 24,1);
-insert into Member(`name`, `age`, `team_id`) values('wiji', 24,2);
-insert into Member(`name`, `age`, `team_id`) values('dosik', 24,2);
-insert into Member(`name`, `age`, `team_id`) values('mingi', 24,3);
-insert into Member(`name`, `age`, `team_id`) values('youngjin', 24,3);
-
-insert into Like_book(`member_id`, `book_id`) values(1,3);
-insert into Like_book(`member_id`, `book_id`) values(1,2);
-insert into Like_book(`member_id`, `book_id`) values(2,4);
-insert into Like_book(`member_id`, `book_id`) values(3,4);
-insert into Like_book(`member_id`, `book_id`) values(5,1);
+insert into Like_book(member_id, book_id) values(1,3);
+insert into Like_book(member_id, book_id) values(1,2);
+insert into Like_book(member_id, book_id) values(2,4);
+insert into Like_book(member_id, book_id) values(3,4);
+insert into Like_book(member_id, book_id) values(5,1);
