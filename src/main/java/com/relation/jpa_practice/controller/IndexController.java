843ae59c4248e76fb778b38c95d688d011c9efa4
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-04-21 02:44:17 +0900

    feat: 부트스트랩 + jquery로 간단한 UI 구현

---
+++ src/main/java/com/relation/jpa_practice/controller/IndexController.java
@@ -0,0 +1,38 @@
+package com.relation.jpa_practice.controller;
+
+import com.relation.jpa_practice.controller.dto.MemberResponseDto;
+import com.relation.jpa_practice.domain.Member;
+import com.relation.jpa_practice.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/")
+    public String index(Model model){
+        List<MemberResponseDto> members = memberService.findall();
+        model.addAttribute("members", members);
+        return "index";
+    }
+
+    @GetMapping("/get/member/{id}")
+    public String findMemberById(Model model, @PathVariable Long id){
+        MemberResponseDto member = memberService.findById(id);
+        model.addAttribute("member", member);
+        return "memberInfo";
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello.html";
+    }
+}
