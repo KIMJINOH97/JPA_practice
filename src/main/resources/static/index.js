2a12f1997d24a31ec0bd243fb4e9a22b4898586e
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-04-22 17:38:11 +0900

    feat: 프론트 delete 구현

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -7,11 +7,40 @@ let main = {
         $('#btn-delete').on('click', function () {
             _this.delete();
         });
+        $('#btn-enroll').on('click', function (){
+            _this.post();
+        })
+    },
+    post: function(){
+      let data = {
+          name: $('#m_name').val(),
+          age : parseInt($('#m_age').val()),
+          address: {
+              city: $('#m_city').val(),
+              gu: $('#m_gu').val(),
+              dong: $('#m_dong').val()
+          },
+          team_name: $('#m_team').val()
+      }
+
+      $.ajax({
+            type: 'POST',
+            url: `/api/member`,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('회원이 등록되었습니다.');
+            window.location.href='/';
+        }).fail(function (e){
+            alert(JSON.stringify(e));
+        });
+
     },
     update: function () {
         let data = {
             name: $('#name').val(),
-            age : $('#age').val(),
+            age : parseInt($('#age').val()),
             city: $('#city').val(),
             gu: $('#gu').val(),
             dong: $('#dong').val(),
